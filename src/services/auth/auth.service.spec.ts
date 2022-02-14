340776060a81121d96fae68e3cde28d78bb10144
Igor Santana <igorsantana33231011@gmail.com>
2022-02-14 16:55:35 -0300

    refactor: add a factory to provide a fake credencials

--- src/services/auth/auth.service.spec.ts
+++ src/services/auth/auth.service.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 
+const makeFakeCredencials = () => ({
+  email: 'any_email@mail.com',
+  password: 'any_password',
+})
+
 describe('AuthService', () => {
   let authService: AuthService;
 
@@ -17,24 +22,16 @@ describe('AuthService', () => {
   });
 
   it('should call login with correct values', () => {
-    const authentication = {
-      email: 'any_email@mail.com',
-      password: 'any_password',
-    }
     const loginSpy = jest.spyOn(authService, 'login');
 
-    authService.login(authentication);
-    expect(loginSpy).toBeCalledWith(authentication)
+    authService.login(makeFakeCredencials());
+    expect(loginSpy).toBeCalledWith(makeFakeCredencials())
   });
 
   it('should throw if login throws', () => {
-    const authentication = {
-      email: 'any_email@mail.com',
-      password: 'any_password',
-    }
     jest.spyOn(authService, 'login').mockRejectedValueOnce(new Error());
 
-    const promise = authService.login(authentication);
+    const promise = authService.login(makeFakeCredencials());
     expect(promise).rejects.toThrow()
   });
 });
