74a79f03e9800d3e4052cf72f2ecb2fc7b504af5
Igor Santana <igorsantana33231011@gmail.com>
2022-02-17 20:33:32 -0300

    fix: set userStub as opcional dependency from CreateUserDto

--- src/app/user/dto/create-user.dto.ts
+++ src/app/user/dto/create-user.dto.ts
@@ -3,7 +3,12 @@ import { User } from '../entities/user.entity'
 
 @InputType()
 export class CreateUserDto {
-  constructor(private readonly user: User) {}
+  constructor(private readonly user?: User) {
+    this.name = this.user?.name
+    this.birthDate = this.user?.birthDate
+    this.email = this.user?.email
+    this.password = this.user?.password
+  }
 
   @Field()
   name: string
