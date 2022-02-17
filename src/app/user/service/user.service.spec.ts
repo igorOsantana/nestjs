e2c30d37ff4a05598472b3086f6ef50f2e7413b8
Igor Santana <igorsantana33231011@gmail.com>
2022-02-17 20:23:40 -0300

    test: ensure "findOne" method from UserService is called

--- src/app/user/service/user.service.spec.ts
+++ src/app/user/service/user.service.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { UserService } from './user.service'
 import { userStub } from '../../../../test/stubs/user.stub'
+import { User } from '../entities/user.entity'
 
 jest.mock('./user.service')
 
@@ -21,11 +22,26 @@ describe('UserService', () => {
         await service.findAll()
         expect(service.findAll).toHaveBeenCalledTimes(1)
       })
-    })
 
-    test('then it should return an array of User on success', async () => {
-      const users = await service.findAll()
-      expect(users).toEqual([userStub()])
+      test('then it should return an array of User on success', async () => {
+        const users = await service.findAll()
+        expect(users).toEqual([userStub()])
+      })
+    })
+  })
+
+  describe('findOne method', () => {
+    describe('when findOne is called', () => {
+      let user: User
+
+      beforeEach(() => {
+        user = userStub()
+      })
+
+      test('then it should be called', async () => {
+        await service.findOne(user.id)
+        expect(service.findOne).toHaveBeenCalledTimes(1)
+      })
     })
   })
 })
